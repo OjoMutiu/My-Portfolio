404bfa117de08a328a8ef7a3ac332728f9540d3a
OjoMutiu <sourcecode.wildtech@gmail.com>
2025-07-02 16:52:14 +0100

    resume

--- lib/constants/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class AppColors{
-  static const Color blackColor = Color(0xFF000000);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color zinc300D4D = Color(0xFFD4D4D8);
-  static const Color zinc5007171 = Color(0xFF71717A);
-  static const Color zinc8002727 = Color(0xFF27272A);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:portfolio/services/dependencies.dart' as dep;
-import 'package:portfolio/screens/home/home_page.dart';
-
-import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,10 +8,7 @@ Future<void> main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await dep.initDep();
+
   runApp(const MyApp());
 }
 
@@ -26,14 +18,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    dep.getAllMethods();
     return GetMaterialApp(
       title: 'Ojo Mutiu',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(),
+      home: Container(),
     );
   }
 }
